b198578b00797431402c91993809a520fcd03599
THIAGO COSTA PALESTINO <thiago.palestino@cs.up.edu.br>
2025-02-26 21:34:56 -0300

    aula 1

---
+++ calc/src/main.java
@@ -0,0 +1,26 @@
+public class main {
+
+/*      String nome;
+        int rg;
+        char sex;
+        float altura; */
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+        Pessoa p = new Pessoa("thiago", 1, 'M', 173);
+
+        System.out.println(p.nome);
+        System.out.println(p.rg);
+        System.out.println(p.sex);
+        System.out.println(p.altura);
+        
+        System.out.println(p.getNome());
+
+        p.setrg(123);
+        
+        System.out.println(p.getrg());
+        System.out.println("adios");
+    }
+}
